2f32e9630b719cbc4b85b382c6f02ae3ff758a6f
H8K56 <drty_hry@protonmail.com>
2025-04-15 15:23:43 +0300

    implemented create_process function for creating fork() and added background processes

--- retrieve.cpp
+++ retrieve.cpp
@@ -5,7 +5,7 @@
 #include <string.h>
 #include <sys/wait.h>
 
-
+// Check if command exits
 int is_builtin(const char* cmd) {
     char command[256];
 
@@ -30,7 +30,23 @@ int redirect_io(char* args[]){
 }
 
 int create_process(char* args[], int background){
+    pid_t pid = fork();
 
+    if (pid < 0) {
+        perror("Fork error");
+        return -1;
+    } else if (pid == 0) {
+        if (execvp(args[0], args) == -1) {
+            perror("Execution error");
+            exit(EXIT_FAILURE);
+        }
+    } else {
+        // background = 0 (foreground process) else (background process);
+        if (!background) {
+            wait(NULL);
+        }
+    }
+    return 0;
 }
 
 void execute_command(char* args[]) {
