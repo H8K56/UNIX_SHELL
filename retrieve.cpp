7e968f08225387d19af504e51fc610515d1687d9
H8K56 <drty_hry@protonmail.com>
2025-04-15 15:10:49 +0300

    initial skeleton of all functions

---
+++ retrieve.cpp
@@ -0,0 +1,31 @@
+#include "retrieve.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/wait.h>
+
+
+int is_builtin(const char* cmd) {
+
+}
+
+int redirect_io(char* args[]){
+
+}
+
+int create_process(char* args[], int background){
+
+}
+
+void execute_command(char* args[]) {
+
+}
+
+void execute_pipeline(char* args1[], char* args2[]) {
+
+}
+
+void handle_builtin(char* args[]) {
+
+}
