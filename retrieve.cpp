84725a2b6cea7790cdc9f9c2358a217ee2084d95
H8K56 <drty_hry@protonmail.com>
2025-04-15 20:56:46 +0300

    added implementation of pipeline function using two processes and  file descriptors

--- retrieve.cpp
+++ retrieve.cpp
@@ -77,7 +77,7 @@ int create_process(char* args[], int background) {
 void execute_command(char* args[]) {
     if (args[0] == NULL) {
         fprintf(stderr, "No command entered\n");
-        return; // No command entered
+        return;
     }
 
     if(is_builtin(args[0]) == 1) {
@@ -89,6 +89,44 @@ void execute_command(char* args[]) {
 }
 
 void execute_pipeline(char* args1[], char* args2[]) {
+    int pipefd[2];
+    pid_t pid1, pid2;
+
+    if (pipe(pipefd) == -1) {
+        perror("Pipe error");
+        return;
+    }
+    // first half of the pipe
+    pid1 = fork();
+    if (pid1 == -1) {
+        perror("Fork error");
+        return;
+    }
+
+    if (pid1 == 0) {
+        dup2(pipefd[1], STDOUT_FILENO);
+        close(pipefd[0]);
+        close(pipefd[1]);
+        execvp(args1[0], args1);
+        perror("Execution error of first command");
+        exit(EXIT_FAILURE);
+    }
+
+    // second half of the pipe
+    pid2 = fork();
+    if (pid2 == -1) {
+        perror("Fork error");
+        return;
+    }
+
+    if (pid2 == 0) {
+        dup2(pipefd[0], STDIN_FILENO);
+        close(pipefd[0]);
+        close(pipefd[1]);
+        execvp(args2[0], args2);
+        perror("Execution error of second command");
+        exit(EXIT_FAILURE);
+    }
     return;
 }
 
