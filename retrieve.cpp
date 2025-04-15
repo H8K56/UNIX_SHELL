473cd73d5cbecada92da7c7d6d80d5132df52f5e
H8K56 <drty_hry@protonmail.com>
2025-04-15 20:20:18 +0300

    added implementation of redirectIO with use of FILE type to read/write files when using > or <

--- retrieve.cpp
+++ retrieve.cpp
@@ -26,11 +26,31 @@ int is_builtin(const char* cmd) {
 }
 
 int redirect_io(char* args[]){
-
+    if (strcmp(args[1], ">") == 0) {
+        FILE* file = fopen(args[2], "w");
+        if (file == NULL) {
+            perror("File opening error");
+            return -1;
+        }
+        dup2(fileno(file), STDOUT_FILENO);
+        fclose(file);
+        args[1] = NULL;
+    } else if (strcmp(args[1], "<") == 0) {
+        FILE* file = fopen(args[2], "r");
+        if (file == NULL) {
+            perror("File opening error");
+            return -1;
+        }
+        dup2(fileno(file), STDIN_FILENO);
+        fclose(file);
+        args[1] = NULL;
+    }
+    return 0;
 }
 
-int create_process(char* args[], int background){
+int create_process(char* args[], int background = 0){
     pid_t pid = fork();
+    int status;
 
     if (pid < 0) {
         perror("Fork error");
@@ -40,23 +60,42 @@ int create_process(char* args[], int background){
             perror("Execution error");
             exit(EXIT_FAILURE);
         }
-    } else {
-        // background = 0 (foreground process) else (background process);
-        if (!background) {
-            wait(NULL);
-        }
+    } else if(pid > 0){
+        // // background = 0 (foreground process) else (background process);
+        // if (!background) {
+        //     wait(NULL);
+        // }
+        wait(&status);
     }
     return 0;
 }
 
 void execute_command(char* args[]) {
+    if (args[0] == NULL) {
+        fprintf(stderr, "No command entered\n");
+        return; // No command entered
+    }
 
+    if(is_builtin(args[0]) == 1) {
+        handle_builtin(args);
+        return;
+    }else {
+        printf("Command does not exist\n");
+    }
 }
 
 void execute_pipeline(char* args1[], char* args2[]) {
-
+    return;
 }
 
 void handle_builtin(char* args[]) {
+    create_process(args, 0);
+    return;
+}
 
+void display_input(char* args[]) {
+    printf("Command: %s\n", args[0]);
+    for (int i = 1; args[i] != NULL; i++) {
+        printf("Argument %d: %s\n", i, args[i]);
+    }
 }
