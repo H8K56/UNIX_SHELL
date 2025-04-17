db58f63edc3ce1c97fa9a3a0b0b76d85e089e526
H8K56 <drty_hry@protonmail.com>
2025-04-17 12:58:19 +0300

    Since first version is working, applying a class structure will be easier

--- retrieve.h
+++ retrieve.h
@@ -1,13 +1,27 @@
 #ifndef RETRIEVE_H
 #define RETRIEVE_H
 
-void execute_command(char* args[]);
-void execute_pipeline(char* args1[], char* args2[]);
-void execute_parallel(char* args[]);
-void handle_builtin(char* args[]);
-int is_builtin(const char* cmd);
-int redirect_io(char* args[]);
-int create_process(char* args[], int background);
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/wait.h>
 
+class CommandExecutor {
+private:
+    static const int MAX_INPUT_LENGTH = 100;
+
+    int is_builtin(const char* cmd);
+    int redirect_io(char* args[]);
+    int create_process(char* args[], int background);
+    void execute_pipeline_internal(char* args1[], char* args2[]);
+    void execute_parallel_internal(char* args[]);
+
+public:
+    void execute_command(char* args[]);
+    void execute_pipeline(char* args1[], char* args2[]);
+    void execute_parallel(char* args[]);
+    void handle_builtin(char* args[]);
+};
 
 #endif
