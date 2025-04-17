6339522c9914a3b47a63fdf0325a4d2d0e394e93
H8K56 <drty_hry@protonmail.com>
2025-04-17 12:42:00 +0300

    refactor: improved input handling and tokenization in main loop

--- main.cpp
+++ main.cpp
@@ -4,34 +4,41 @@
 #include <sys/wait.h>
 #include <string.h>
 #include <iostream>
+#include "retrieve.cpp"
 
 #define MAX_INPUT_LENGTH 100
 
-int main(){
+int main() {
     char input[MAX_INPUT_LENGTH];
     char* args[MAX_INPUT_LENGTH];
-    int status;
 
-    while(true){
+    while (true) {
         std::cout << "wish> ";
-        std::cin.getline(input, sizeof(input)); // user input
+        if (!std::cin.getline(input, MAX_INPUT_LENGTH)) {
+            std::cout << "\n";
+            break; // EOF
+        }
 
+        // Tokenize input into args
         char* input_ptr = input;
-
-        /*Tokenize the input*/
-        char* token = strsep(&input_ptr, " \n");
+        char* token;
         int i = 0;
 
-        while(token != NULL && i < MAX_INPUT_LENGTH){
-            args[i] = token;
-            token = strsep(&input_ptr, " \n");
-            i++;
+        while ((token = strsep(&input_ptr, " \t")) != NULL) {
+            if (*token == '\0') continue;
+            args[i++] = token;
         }
-        args[i] = NULL; /*Null-terminate the argument list*/
-        
-        if(strcmp(args[0], "exit") == 0){
+        args[i] = NULL;
+
+        if (i == 0) continue;
+
+        if (strcmp(args[0], "exit") == 0) {
             std::cout << "Exiting the shell...\n";
             break;
         }
+
+        handle_builtin(args);
     }
+
+    return 0;
 }
